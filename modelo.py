75129b485f0fcde8685368787ac135596b3f4005
alexotalvaro <alejandromilles@outlook.com>
2026-03-01 23:29:54 +0100

    Modelo: Método de conectar con la base de datos con retorno de la conexion creada para su uso en otros metodos

--- modelo.py
+++ modelo.py
@@ -1,12 +1,25 @@
+from tkinter import messagebox
+
+import mysql.connector
+
+
 class InventarioModelo:
     def __init__(self):
-        # Aquí irá la creación de la Base de Datos
-        pass
+        self.conexion= None
+        self.conectar()
 
-    def obtener_productos(self):
-        # Esto devuelve una lista
-        pass
 
-    def insertar_productos(self):
-        # Esto añade productos
-        pass
+    def conectar(self):
+        try:
+            conexion = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="",
+                database="tienda"
+            )
+            self.conexion = conexion
+        except mysql.connector.Error as error:
+            messagebox.showerror("Error de conexión",f"No se pudo conectar a la base de datos: {error}")
+            return None
+
+        return conexion
